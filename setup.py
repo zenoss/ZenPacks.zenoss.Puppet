528f901ecfb4b29fe68a5b58a6f5df208abd1347
Kells Kearney <kkearney@zenoss.com>
2012-10-15 15:01:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.Puppet"
-VERSION = "1.0.1"
+VERSION = "1.0.2"
 AUTHOR = ""
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
